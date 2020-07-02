97367c4bc558452b6aa8a9b7dde86f6b5ef3b89d
borjilla <47045714+borjilla@users.noreply.github.com>
2020-07-02 23:14:08 +0200

    Add files via upload

---
+++ Proyectos/7_Servo/servo/servo.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+Servo miservo;
+int i = 0;
+void setup() {
+       miservo.attach(3);
+}
+void loop(){
+      //Va de 0 a 180 grados en pasos de un grado
+      for(i = 0; i < 180; i++)   {
+                 miservo.write(i);
+                 //Esperamos a que el servo alcance la nueva posición
+                 delay(15);
+      }
+      //De 180 a 0
+      for(i = 180; i>=1; i--) {
+} }
