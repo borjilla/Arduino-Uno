de45ed6a794e2fb2d6f7833ed032df7533f57661
borjilla <47045714+borjilla@users.noreply.github.com>
2020-10-02 23:35:09 +0200

    Add files via upload

--- Proyectos/8_Servo+Potenciometro/servo_potenciometro/servo_potenciometro.ino
+++ Proyectos/8_Servo+Potenciometro/servo_potenciometro/servo_potenciometro.ino
@@ -1,16 +1,19 @@
 #include <Servo.h>
 Servo miservo;
+int valorPot = 2;
 void setup() {
-       Serial.begin(9600);
-       miservo.attach(3);
+     miservo.attach(3);
 }
-void loop(){
-       long angulo;
-       if (Serial.available() > 0) {
-        angulo=Serial.parseInt();
-        if (angulo > 0 && angulo < 180){
-          miservo.write(angulo);
-          delay(15);
-        }
-       }
+void loop() {
+     valorPot = analogRead(2);
+/*Los valores de analogRead() van desde 0 a 1023 y los valores
+aceptados por miservo.write() van desde 0 a 180, por eso reajustamos 
+el valor leído para poderlo utilizar con el servomotor. En este caso 
+particular, como los mínimos de ambos rangos son 0, en vez de map() 
+podríamos haber escrito una simple regla de proporcionalidad, mediante 
+la fórmula valorPot=valorPot*(180.0/1023.0)*/
+
+     valorPot = map(valorPot, 0, 1023, 0, 180);
+     miservo.write(valorPot);
+     delay(15);  //Para dar tiempo al servo a moverse
 }
